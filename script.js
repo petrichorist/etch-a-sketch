b88697d52711c4724eb2d6e4cde4d8e425b7e612
pwnapples <2dsaulgoodmanbecomes3d@gmail.com>
2024-11-23 13:14:21 +0500

    REFACTOR: Remove comments and change function names to appropriate counterparts

--- script.js
+++ script.js
@@ -1,19 +1,5 @@
 'use strict';
 
-// const container = document.getElementById("container");
-
-// function makeRows(rows, cols) {
-//   container.style.setProperty('--grid-rows', rows);
-//   container.style.setProperty('--grid-cols', cols);
-//   for (let c = 0; c < (rows * cols); c++) {
-//     let cell = document.createElement("div");
-//     cell.innerText = (c + 1);
-//     container.appendChild(cell).className = "grid-item";
-//   };
-// };
-
-// makeRows(16, 16);
-
 const container = document.getElementById('container');
 const clear = document.getElementById('clear');
 const overwrite = document.getElementById('overwrite');
@@ -21,10 +7,10 @@ const overwrite = document.getElementById('overwrite');
 overwrite.addEventListener('click', () => {
   let pixels = prompt('Enter the amount of pixels');
   container.style.width = pixels;
-  ligma();
+  draw();
 });
 
-function ligma() {
+function draw() {
   container.textContent = '';
   
   for (let i = 0; i < 224; i++) {
@@ -40,4 +26,4 @@ function ligma() {
   }
 }
 
-ligma();
+draw();
