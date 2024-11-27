802db60891951b2cb66154815cb0e2d62fd8c3d2
pwnapples <2dsaulgoodmanbecomes3d@gmail.com>
2024-11-27 15:19:16 +0500

    feat: Improve the UI

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 const container = document.getElementById('container');
 const clear = document.getElementById('clear');
 const overwrite = document.getElementById('overwrite');
+let opacity = 0.0;
 
 overwrite.addEventListener('click', () => {
   let pixels = prompt('Enter the amount of pixels');
@@ -12,16 +13,22 @@ overwrite.addEventListener('click', () => {
 
 function draw() {
   container.textContent = '';
-  
+
   for (let i = 0; i < 224; i++) {
     const cell = document.createElement('div');
     container.appendChild(cell).className = 'item';
     cell.addEventListener('mouseover', () => {
-      cell.style.backgroundColor = '#000';
+      let first = Math.floor(Math.random() * 255);
+      let second = Math.floor(Math.random() * 255);
+      let third = Math.floor(Math.random() * 255);
+      opacity += 0.1;
+      cell.style.opacity = `${opacity}`;
+      cell.style.backgroundColor = `rgb(${first}, ${second}, ${third})`;
     });
 
     clear.addEventListener('click', () => {
       cell.style.backgroundColor = '#FFF';
+      opacity = 0.1;
     });
   }
 }
